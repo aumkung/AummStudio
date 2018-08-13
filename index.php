e38a612ee705b6780b3d6759c81e8029d13b6db9
Tassanai <amm_25204@hotmail.com>
2018-08-13 22:50:59 +0700

    เพิ่ม index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-	phpinfo();
+<html>
+	<head>
+		<title>AummStudio</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<?php phpinfo(); ?>
+	</body>
+</html>
 
